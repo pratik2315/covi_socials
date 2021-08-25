0595749603a6f4e317fa347b1432d4f3739e9d49
pratik2315 <pratikhaldankar360@gmail.com>
2021-08-25 17:11:01 +0530

    added a login feature

--- app/src/main/java/com/example/covisocials/LoginActivity.kt
+++ app/src/main/java/com/example/covisocials/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.covisocials
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.covisocials.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -13,7 +15,22 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            
+            val userEmail = binding.etEmail.text.toString()
+            val userPassword = binding.etPassword.text.toString()
+            if (userEmail.isBlank() || userPassword.isBlank()){
+                Toast.makeText(this, "Input fields Cannot be Empty!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(this, "Task Failed!", Toast.LENGTH_SHORT).show()
+
+                }
+            }
         }
     }
 }
